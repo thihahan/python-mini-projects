07ce672cffb098da8bc40fa1a0371a82ae4ff734
Thiha <76861847+Thiha5574@users.noreply.github.com>
2022-03-17 18:31:22 +0630

    Delete mini projects directory

--- mini projects/madlibs.py
+++
@@ -1,8 +0,0 @@
-adj = input("adjective: ")
-verb1 = input("verb 1: ")
-verb2 = input("verb2: ")
-favourite_person = input("Enter your favourite person: ")
-madlib = f"computer programming is so {adj}! it makes me so exited all the time because " \
-         f"I love to {verb1}. Stay hydrated and {verb2} like you are {favourite_person}."
-if __name__ == '__main__':
-    print(madlib)
--- mini projects/words_by_me.py
+++
@@ -1,1 +0,0 @@
-words = ['apple', 'orange', 'banana', 'laptop', 'phone', 'easy', 'good', 'bad', 'better', 'special', 'clever']
